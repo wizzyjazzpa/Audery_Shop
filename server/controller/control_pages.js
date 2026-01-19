697d6644191ec32d83dd5403eb38e00c91b60c1c
OGM <walisonmichael8@gmail.com>
2026-01-19 16:14:25 +0100

    new view

--- server/controller/control_pages.js
+++ server/controller/control_pages.js
@@ -4,3 +4,24 @@ exports.home = async(req,res)=>{
     }
     res.render('home',locals);
 }
+exports.about = async(req,res)=>{
+      const locals ={
+           title:"About"
+    }
+    res.render('about',locals)
+}
+
+exports.contact = async(req,res)=>{
+      const locals ={
+           title:"Contact"
+    }
+    res.render('contact',locals)
+}
+
+
+exports.singleProduct = async(req,res)=>{
+      const locals ={
+           title:"Single Product"
+    }
+    res.render('product',locals)
+}
--- server/routes/routePages.js
+++ server/routes/routePages.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const controlPages = require('../controller/control_pages');
 
 router.get('/',controlPages.home);
-
+router.get('/about',controlPages.about);
+router.get('/contact',controlPages.contact);
+router.get('/product',controlPages.singleProduct);
 module.exports = router;
